64da79eb7f3d8ad0ff20c7e42adf5400919ea26e
juaseaya <152338292+JuanAyala619@users.noreply.github.com>
2026-01-07 02:18:08 +0000

    incorporacion_put_patch_y_delete

--- demo_rest_api/views.py
+++ demo_rest_api/views.py
@@ -29,12 +29,46 @@ class DemoRestApi(APIView):
         data['is_active'] = True
         data_list.append(data)
         return Response({'message': 'Dato guardado exitosamente.', 'data': data}, status=status.HTTP_201_CREATED)
-    
-    def put(self,request.id):
+
+class DemoRestApiItem(APIView):    
+    def put(self,request,id):
         data=request.data
-        id_user= data['id']
+        required_fields = ['name', 'email', 'is_active']
+        for field in required_fields:
+            if field not in data:
+                return Response({'error': f'Falta el campo requerido: {field}'}, status=status.HTTP_400_BAD_REQUEST)
+    
         for item in data_list:
-            if item.get('id', id_user):
+            id_user=item['id']
+            if id_user==id:
                 item['name']=data['name']
                 item['email']=data['email']
                 item['is_active']=data['is_active']
+                return Response({'message': 'Dato actualizados.','data':item}, status=status.HTTP_200_OK)
+        return Response({'error': 'ID no encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def patch(self,request,id):
+        data=request.data
+        if not data:
+            return Response({'error': 'No se proporcionaron campos para actualizar'}, status=status.HTTP_400_BAD_REQUEST)
+        for item in data_list:
+            id_user=item['id']
+            if id_user==id:
+                if 'name' in data:
+                    item['name'] = data['name']
+                if 'email' in data:
+                    item['email'] = data['email']
+                if 'is_active' in data:
+                     item['is_active'] = data['is_active']
+                return Response({'message': 'Datos actualizados parcialmente.', 'data': item}, status=status.HTTP_200_OK)
+        return Response({'error': 'ID no encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self,request,id):
+        for item in data_list:
+            if item['id'] == id:
+                if item.get('is_active', False):
+                    item['is_active'] = False
+                    return Response({'message': 'Elemento desactivado exitosamente.', 'data': item}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'message': 'El elemento ya estaba inactivo.'}, status=status.HTTP_200_OK)
+
